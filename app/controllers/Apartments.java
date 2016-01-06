9e21ca09de5edb05073316eba5accaa6a647729e
atabari <ajla@tabari.me>
2016-01-06 22:31:36 +0100

    update apartment

--- app/controllers/Apartments.java
+++ app/controllers/Apartments.java
@@ -1,23 +1,28 @@
 package controllers;
 
 import models.Apartment;
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.apartment;
 import views.html.createapartment;
+import views.html.updateapartment;
 
 /**
  * Created by User on 12/29/2015.
  */
 public class Apartments extends Controller {
 
-    public Result renderApartment() {
-        return ok(createapartment.render());
-    }
 
+    // Apartment details
     public Result apartment(Integer apartmentId) {
         Apartment apart = Apartment.getApartmentById(apartmentId);
         return ok(apartment.render(apart));
+    }
+
+    // Create apartment
+    public Result renderApartment() {
+        return ok(createapartment.render());
     }
 
     public Result createApartment() {
@@ -31,5 +36,24 @@ public class Apartments extends Controller {
         }
     }
 
+    // Update apartment
+    public Result renderUpdateApartment(Integer apartmentId) {
+        Apartment apart = Apartment.getApartmentById(apartmentId);
+
+        Logger.debug(apart.toString());
+        
+        return ok(updateapartment.render(apart));
+    }
+
+    public Result updateApartment() {
+        Apartment apart = Apartment.updateApartment();
+        if (apart != null) {
+            flash("success", "Uspješno ste ažurirali podatke o  apartmanu.");
+            return ok(apartment.render(apart));
+        } else {
+            flash("error", "Desila se greška, podaci o apartmanu nisu ažurirani.");
+            return ok(createapartment.render());
+        }
+    }
 
 }
